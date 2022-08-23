4d1f15fc07b1cd399b2bdb1d094eccda86bba6c4
Michael Chong <mikec0920@gmail.com>
2022-08-22 22:17:45 -0700

    Associate events to creator

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,5 @@
 class Event < ApplicationRecord
   validates :title, :date, :location, presence: true
 
-  belongs_to :creators, class_name: "User"
+  belongs_to :creator, class_name: "User"
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_12_001208) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_22_185200) do
   create_table "events", force: :cascade do |t|
     t.string "title"
     t.datetime "date"
     t.string "location"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "creator_id"
   end
 
   create_table "users", force: :cascade do |t|
