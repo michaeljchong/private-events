655dd535d551d8979a23b3ed4fe8ee1d30d1bd12
Michael Chong <mikec0920@gmail.com>
2022-08-26 22:47:23 -0700

    Add association between event and attendee

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,4 +2,6 @@ class Event < ApplicationRecord
   validates :title, :date, :location, presence: true
 
   belongs_to :creator, class_name: "User"
+  has_many :attendances, foreign_key: :attendee_id
+  has_many :attendees, through: :attendances
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_22_185200) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_27_044024) do
+  create_table "attendances", force: :cascade do |t|
+    t.integer "attendee_id"
+    t.integer "attended_event_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "events", force: :cascade do |t|
     t.string "title"
     t.datetime "date"
