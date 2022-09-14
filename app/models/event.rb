790d0d3d90b8303891918941b4ebcc88654934e9
Michael Chong <mikec0920@gmail.com>
2022-09-13 22:53:18 -0700

    Organize past and upcoming events

--- app/models/event.rb
+++ app/models/event.rb
@@ -4,4 +4,12 @@ class Event < ApplicationRecord
   belongs_to :creator, class_name: "User"
   has_many :attendances, foreign_key: :attended_event_id
   has_many :attendees, through: :attendances
+
+  def self.upcoming
+    where("date > ?", Time.now)
+  end
+
+  def self.past
+    where("date < ?", Time.now)
+  end
 end
