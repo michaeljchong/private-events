be4a3529a479620916378e75c6549bf1d2132bab
Michael Chong <mikec0920@gmail.com>
2022-09-14 17:40:31 -0700

    Fix bug in upcoming and past events

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,7 @@ class UsersController < ApplicationController
   before_action :authenticate_user!
   
   def show
-    @events = current_user.created_events
+    @created_events = current_user.created_events
+    @attended_events = current_user.attended_events
   end
 end
