111bb0e33ec866ccf45a8baf95e53be969051af0
Michael Chong <mikec0920@gmail.com>
2022-09-18 01:02:50 -0700

    Migrate database from sqlite3 to postgres

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,9 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 2022_08_27_044024) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "attendances", force: :cascade do |t|
     t.integer "attendee_id"
     t.integer "attended_event_id"
